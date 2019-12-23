8a1410dd710703e0653368ae58b4e3f58bd21bb2
Dominic Streiff <dostreif@ethz.ch>
2019-12-23 16:04:28 +0100

    fixed bugs in postprocessing of features, descriptors still missing

--- src/net_inference/src/inference.cpp
+++ src/net_inference/src/inference.cpp
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <iostream>
 #include <math.h>
+#include <cv.hpp>
+#include <chrono>
 
 #include "tensorflow/cc/ops/const_op.h"
 #include "tensorflow/cc/ops/image_ops.h"
@@ -18,7 +20,6 @@
 #include "tensorflow/core/public/session.h"
 #include "tensorflow/core/util/command_line_flags.h"
 
-#include <cv.hpp>
 
 // These are all common classes it's handy to reference with no namespace.
 using tensorflow::int32;
@@ -80,8 +81,8 @@ Status readTensorFromMat(const Mat &mat, Tensor &outTensor) {
 
     // Trick from https://github.com/tensorflow/tensorflow/issues/8033
     float *p = outTensor.flat<float>().data();
-    Mat fakeMat(mat.rows, mat.cols, CV_32FC3, p);
-    mat.convertTo(fakeMat, CV_32FC3);
+    Mat fakeMat(mat.rows, mat.cols, CV_32FC1, p);
+    mat.convertTo(fakeMat, CV_32FC1);
 
     auto input_tensor = Placeholder(root.WithOpName("input"), tensorflow::DT_FLOAT);
     vector<pair<string, tensorflow::Tensor>> inputs = {{"input", outTensor}};
@@ -102,14 +103,33 @@ Status readTensorFromMat(const Mat &mat, Tensor &outTensor) {
     return Status::OK();
 }
 
+// returns row and column indeces and heatmap score where the input array is true
+void whereTrue(Eigen::Tensor<bool, 2, 1>& mask, Eigen::Tensor<float, 2, 1>& heatmap, vector<unsigned int>& r_ind, vector<unsigned int>& c_ind, vector<float>& scores)
+{
+    auto dims = mask.dimensions();
+    for (int i = 0; i<dims[0]; i++)
+    {
+        for (int j = 0; j<dims[1]; j++)
+        {
+            if (mask(i,j))
+            {
+                r_ind.push_back(i);
+                c_ind.push_back(j);
+                scores.push_back(heatmap(i,j));
+            }
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
-    string root_dir = "";
-    string graph = "src/net_inference/src/half_enc_half_dec.pb";
-    string image_path = "/media/dominic/Extreme SSD/datasets/gray_images/eth3d_gray/DSC_0362.png";
-    int32 input_width = 640;
-    int32 input_height = 480;
-    string input_layer = "input";
-    vector<string> output_layer = {"heat/BiasAdd", "point_mask/mul", "desc/BiasAdd"};
+    // define some constants
+    const string root_dir = "";
+    const string graph = "src/net_inference/src/half_enc_half_dec.pb";
+    const string image_path = "/media/dominic/Extreme SSD/datasets/training_set/-000000-1540457005_400.png";
+    const int32 input_width = 640;
+    const int32 input_height = 480;
+    const string input_layer = "input";
+    const vector<string> output_layer = {"heat/BiasAdd", "point_mask/mul", "desc/BiasAdd"};
 
     // First we load and initialize the model.
     std::unique_ptr<tensorflow::Session> session;
@@ -120,6 +140,7 @@ int main(int argc, char* argv[]) {
     return -1;
     }
 
+    // read image from harddisk as uint8 image
     Mat image;
     image = imread( image_path, 0);
     if ( !image.data )
@@ -127,77 +148,100 @@ int main(int argc, char* argv[]) {
         printf("No image data \n");
         return -1;
     }
+    // convert image to float32 in range [0,1]
+    Mat grayim;
+    image.convertTo(grayim, CV_32FC1);
+    grayim /= 255;
 
+    // initialize input tensor
     tensorflow::TensorShape shape = tensorflow::TensorShape();
     shape.AddDim(1);
     shape.AddDim(input_height);
     shape.AddDim(input_width);
     shape.AddDim(1);
-
     Tensor tensor;
     tensor = Tensor(tensorflow::DT_FLOAT, shape);
-    Status readTensorStatus = readTensorFromMat(image, tensor);
+    // read image into tensor
+    Status readTensorStatus = readTensorFromMat(grayim, tensor);
     if (!readTensorStatus.ok()) {
         LOG(ERROR) << "Mat->Tensor conversion failed: " << readTensorStatus;
         return -1;
     }
 
-
     // Actually run the image through the model.
     std::vector<Tensor> outputs;
-    Status run_status = session->Run({{input_layer, tensor}},
-                                   {output_layer}, {}, &outputs);
+    Status run_status = session->Run({{input_layer, tensor}},{output_layer}, {}, &outputs);
     if (!run_status.ok()) {
     LOG(ERROR) << "Running model failed: " << run_status;
     return -1;
     }
+    // output of network is in row major format
     Eigen::Tensor<float, 4, 1> heat = outputs[0].tensor<float, 4>();
+    Eigen::Tensor<bool, 4, 1> mask = outputs[1].tensor<float, 4>().cast<bool>();
+    // softmax along channel dimension
     heat = heat.exp();
-    cout << "heat size: (" << heat.dimensions()[0] << ", " << heat.dimensions()[1] << ", " << heat.dimensions()[2] << ", " << heat.dimensions()[3] << ")\n";
-
     Eigen::array<int, 1> dims({3});
     Eigen::array<int, 4> rep({1, 1, 1, 65});
     Eigen::array<int, 4> four_dim({1, 60, 80, 1});
-    Eigen::Tensor<float, 4, 1> t_norm = heat.sum(dims).eval().reshape(four_dim).eval().broadcast(rep);
-    cout << "t_norm size: (" << t_norm.dimensions()[0] << ", " << t_norm.dimensions()[1] << ", " << t_norm.dimensions()[2] << ", " << t_norm.dimensions()[3] << ")\n";
-
+    Eigen::Tensor<float, 4, 1> t_norm = heat.sum(dims).reshape(four_dim).broadcast(rep);
     heat = heat / t_norm;
 
-//    dense = np.reshape(dense, [N, Hc, Wc, 8, 8])
-//    dense = np.transpose(dense, [0, 1, 3, 2, 4])
-//    heatmap = np.reshape(dense, [N, Hc * 8, Wc * 8])
-
+    // reshape heatmap into 480x640 tensor after removing dustbin channel
     Eigen::array<int, 4> offset({0, 0, 0, 0});
     Eigen::array<int, 4> extent({1, 60, 80, 64});
     Eigen::Tensor<float, 4, 1> nodust = heat.slice(offset, extent);
-
-    cout << "nodust size: (" << nodust.dimensions()[0] << ", " << nodust.dimensions()[1] << ", " << nodust.dimensions()[2] << ", " << nodust.dimensions()[3] << ")\n";
-
     Eigen::array<int, 5> five_dim({1, 60, 80, 8, 8});
     array<int, 5> shuffle_five({0, 1, 3, 2, 4});
     Eigen::array<int, 2> two_dim({480, 640});
-
     Eigen::Tensor<float, 5, 1> heatmap_5 = nodust.reshape(five_dim).eval().shuffle(shuffle_five);
-    cout << "heat_5 size: (" << heatmap_5.dimensions()[0] << ", " << heatmap_5.dimensions()[1] << ", " << heatmap_5.dimensions()[2] << ", " << heatmap_5.dimensions()[3]<< ", " << heatmap_5.dimensions()[4] << ")\n";
-
     Eigen::Tensor<float, 2, 1> heatmap = heatmap_5.reshape(two_dim);
-    cout << "heatmap size: (" << heatmap.dimensions()[0] << ", " << heatmap.dimensions()[1] << ")\n";
-    
-    Mat out(input_height, input_width, CV_32F, heatmap.data());
 
-//    eigen2cv()
+    // reshape point_mask into 480x640 tensor
+    Eigen::Tensor<bool, 5, 1> mask_5 = mask.reshape(five_dim).eval().shuffle(shuffle_five);
+    Eigen::Tensor<bool, 2, 1> point_mask = mask_5.reshape(two_dim);
 
+    // get indeces of feature points and corresponding heatmap score
+    vector<unsigned int> r_ind;
+    vector<unsigned int> c_ind;
+    vector<float> scores;
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+    whereTrue(point_mask, heatmap, r_ind, c_ind, scores);
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << endl;
+
+    // convert to cv2 matrix for display
+    heatmap = heatmap.clip(0.001, 1);
+    heatmap = heatmap.log();
+    Mat heat_gray(input_height, input_width, CV_32F, heatmap.data());
+    // normalize heatmap for display
     double min, max;
-    minMaxLoc(out, &min, &max);
-    // cout << min << endl;
-    // cout << max << endl;
-    out = (out - min) / (max - min);
+    minMaxLoc(heat_gray, &min, &max);
+    heat_gray = (heat_gray - min) / (max - min) * 255;
+    // apply colormap
+    Mat tmp;
+    heat_gray.convertTo(tmp, CV_8UC1);
+    Mat heat_jet;
+    applyColorMap(tmp, heat_jet, COLORMAP_JET);
+    // overlay image and heatmap
+    Mat img_out;
+    Mat in[] = {image, image, image};
+    merge(in, 3, img_out);
+    Mat out_heat;
+    addWeighted(heat_jet, 0.7, img_out, 0.3, 0, out_heat);
 
-//    cout << "M = " << endl << " "  << out << endl << endl;
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    // draw circles corresponding to feature locations
+    Mat out_circ = img_out.clone();
+    for(int i = 0; i < scores.size(); i++)
+    {
+        Point pt(c_ind.at(i), r_ind.at(i));
+        circle(out_circ, pt, 1, Scalar(0, 255, 0), FILLED, LINE_AA);
+    }
+    Mat out;
+    hconcat(out_heat, out_circ, out);
+    // show output
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
     imshow("Display Image", out);
     waitKey(0);
 
-//    ros::init(argc, argv, "super_mario_net_inference");
     return 0;
 }
